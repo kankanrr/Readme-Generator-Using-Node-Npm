82cd359d19d941f7c2d9df00c53a77e6c9449148
Sameer Mirza <srmirza14@gmail.com>
2023-06-08 22:08:28 -0400

    set up readmeQUestions array

--- index.js
+++ index.js
@@ -5,6 +5,50 @@ const fs = require('fs');
 
 // Readme Gen Questions
 
+const readmeQuestions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'Project Title:'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Project Description:'
+    },
+    {
+        name: 'install',
+        type: 'input',
+        message: 'Project Usagee:'
+    },
+    {
+        name: 'license',
+        type: 'input',
+        message: 'License:',
+        choices: ['MIT License','Creative Commons','GMU', '']
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: 'Contribution Guidelines:'
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Test Instructions:'
+    },
+    {
+        name: 'githubUsername',
+        type: 'input',
+        message: 'Github Username:'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Email:'
+    }
+];
+
 // Readme Gen Creation
 
 // Initialization
