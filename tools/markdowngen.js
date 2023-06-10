1137d1ed8c9a21ca156e9ba39fccb4b013011fef
Sameer Mirza <srmirza14@gmail.com>
2023-06-09 20:10:32 -0400

    finished renderBadge function

---
+++ tools/markdowngen.js
@@ -0,0 +1,7 @@
+// Badge render function
+
+function renderBadge (license) {
+    if (license === 'MIT License') {
+        return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    }
+}
