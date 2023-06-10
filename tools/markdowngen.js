11f0fe158ee83da39c016b6a936fe0dc1eab934f
Sameer Mirza <srmirza14@gmail.com>
2023-06-09 20:14:39 -0400

    finished renderLicenseSection function

--- tools/markdowngen.js
+++ tools/markdowngen.js
@@ -3,5 +3,32 @@
 function renderBadge (license) {
     if (license === 'MIT License') {
         return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
-    }
+    } else if (license === 'Creative Commons') {
+        return '![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)';
+    } else if (license === 'GMU') {
+        return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    } else return '';
 }
+
+// license link func
+
+function renderLink (license) {
+    if (license === 'MIT License') {
+        return '(https://opensource.org/licenses/MIT)';
+    } else if (license === 'Creative Commons') {
+        return '(http://creativecommons.org/publicdomain/zero/1.0/)';
+    } else if (license === 'GMU') {
+        return '(https://www.gnu.org/licenses/gpl-3.0)';
+    } else return '';
+}
+
+function renderLicenseSection (license) {
+    if (license === 'MIT License') {
+        return 'MIT License';
+    } else if (license === 'Creative Commons') {
+        return 'Creative Commons';
+    } else if (license === 'GMU') {
+        return 'GMU';
+    } else return '';
+}
+
